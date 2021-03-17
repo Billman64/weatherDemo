d65d90260f742b08d740a590bb56fc5e578b7e3c
Bill <striderbillman@gmail.com>
2021-03-17 16:37:53 -0400

    Current weather data now received and displayed.

--- app/src/main/java/com/twitter/challenge/view/MainActivity.kt
+++ app/src/main/java/com/twitter/challenge/view/MainActivity.kt
@@ -5,9 +5,11 @@ import kotlinx.coroutines.*
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import com.twitter.challenge.R
 import com.twitter.challenge.TemperatureConverter
+import com.twitter.challenge.TemperatureConverter.celsiusToFahrenheit
 import com.twitter.challenge.model.CurrentWeatherAPI
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Retrofit
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val temperatureView: TextView = findViewById(R.id.temperature) as TextView
+        val temperatureView: TextView = findViewById(R.id.temperatureC) as TextView
         temperatureView.setText(getString(R.string.temperature, 34f, TemperatureConverter.celsiusToFahrenheit(34f)))
 
         getWeatherData()
@@ -73,6 +75,7 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "Coroutine for network call")
 
             try{
+                // get data
                 Log.d(TAG, "getting weather...")
                 val responseCurrentWeather = currentWeatherApi.getCurrentWeather().awaitResponse()
                 Log.d(TAG, "response: ${responseCurrentWeather.code()} |  ${responseCurrentWeather.message()}")
@@ -80,23 +83,32 @@ class MainActivity : AppCompatActivity() {
                 val data = responseCurrentWeather.body()!!.getAsJsonObject()
                 Log.d(TAG, " data length: ${data.toString().substring(0,50)}")
 
+                // parse data
+                val tempJson = data.asJsonObject.getAsJsonObject("weather")
+                val temp = tempJson.getAsJsonPrimitive("temp").toString()
+
+                val windJson = data.getAsJsonObject("wind")
+                val wind = windJson.getAsJsonPrimitive("speed").toString()
+
+                val cloudsJson = data.getAsJsonObject("clouds")
+                val cloudiness = cloudsJson.getAsJsonPrimitive("cloudiness").toString()
+
+
                 // update UI
                 withContext(Dispatchers.Main){
-                    var tempJson = data.asJsonObject.getAsJsonObject("weather")
-                    var temp = tempJson.getAsJsonPrimitive("temp").toString()
 
-                    temperature.text = temp
+                    temperatureC.text = temp + " C"
+                    temperatureF.text = celsiusToFahrenheit(temp.toFloat()).toString() + " F"
 
+                    speed.text = wind
 
+                    if(cloudiness.toFloat()>50f) weatherSymbol.visibility = View.VISIBLE
                 }
 
             } catch (e:java.lang.Exception){
                 Log.e(TAG, " network error: ${e.message}")
 
-
             }
-
-
 
         }
     }
